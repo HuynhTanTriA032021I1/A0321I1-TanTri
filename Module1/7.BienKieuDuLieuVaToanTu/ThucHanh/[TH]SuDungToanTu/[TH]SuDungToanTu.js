9588231560ad433623a9fd3adc15270ef819ec33
Huỳnh Tán Trí <httri1418@gmail.com>
2021-04-19 21:20:09 +0700

    nb

---
+++ Module1/7.BienKieuDuLieuVaToanTu/ThucHanh/[TH]SuDungToanTu/[TH]SuDungToanTu.js
@@ -0,0 +1,11 @@
+let inputWidth;
+let inputHeight;
+
+inputWidth = prompt("Nhập width");
+inputHeight = prompt("Nhập height");
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+let area = width * height;
+document.write("Diện Tích: " + area);
